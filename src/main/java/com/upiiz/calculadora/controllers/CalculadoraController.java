0f46d79f835c012444c2c8e63168f1e931956a36
Patricia <patriciavc8991@gmail.com>
2026-05-07 18:41:58 -0600

    actualización

--- src/main/java/com/upiiz/calculadora/controllers/CalculadoraController.java
+++ src/main/java/com/upiiz/calculadora/controllers/CalculadoraController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class CalculadoraController {
-    @GetMapping()
+    @GetMapping("/")
     public String calculadora(){
         return "calculadora";
     }
